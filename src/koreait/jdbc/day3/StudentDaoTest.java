edce075f5abda70e3d4ddc370d7fe95901f62c97
kimsohee-around <koreait.sec2020@gmail.com>
2023-06-13 11:18:49 +0900

    #8 dao 테스트

--- src/koreait/jdbc/day3/StudentDaoTest.java
+++ src/koreait/jdbc/day3/StudentDaoTest.java
@@ -8,7 +8,7 @@ public class StudentDaoTest {
 		StudentDao dao = new StudentDao();
 		
 		
-		System.out.println("1. insert 테스트");
+		System.out.println(":::::::1. insert 테스트:::::::");
 		System.out.println("2023009 김땡구 17 강남구 - 데이터 입력");
 		StudentDto dto = new StudentDto("2023009", "김땡구", 17, "강남구"); 
 		try {
@@ -18,14 +18,28 @@ public class StudentDaoTest {
 			System.out.println("예외 - " + e.getMessage());
 		}
 		
-		System.out.println("2. update 테스트");
+		System.out.println("\n:::::::2. update 테스트:::::::");
 		System.out.println("2023009 김땡구를 16 용산구 - 데이터 수정");
 		dto = new StudentDto("2023009", "김땡구", 16, "용산구");
+		try {
+			int count = dao.update(dto);
+			System.out.println("학생 등록 " + count + "건 정보 수정 성공!!");
+		} catch (SQLException e) {
+			System.out.println("예외 - " + e.getMessage());
+
+		}
 		
 		
-		
-		System.out.println("3. delete 테스트");
+		System.out.println("\n:::::::3. delete 테스트:::::::");
 		System.out.println("2023009 - 데이터 삭제");
+		try {
+			int count = dao.delete("2023009");
+			System.out.println("학생 등록 " + count + "건 삭제 성공!!");
+
+		} catch (SQLException e) {
+			System.out.println("예외 - " + e.getMessage());
+
+		}
 	}
 
 }
